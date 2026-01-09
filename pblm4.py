bd5e705a7146251177ab537a211ca4e41e244d2c
sejalkashyap97 <sejalkashyap97@gmail.com>
2026-01-09 23:04:46 +0530

    added problem 4

---
+++ pblm4.py
@@ -0,0 +1,14 @@
+# Write a program to crop out the words that are palindromes from a sentence given
+# as user input
+def is_palindrome(wd):
+    if wd==wd[::-1]:
+        return True
+    else:   
+        return False
+def removing_palindrome(sentence):
+    word=sentence.split()
+    for i in word: 
+        if is_palindrome(i):
+            sentence=sentence.replace(i,"")
+    print(sentence)
+removing_palindrome("madam arora teaches malayalam")
