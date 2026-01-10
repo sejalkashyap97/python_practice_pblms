3fc4e1a7e59ceb39cfedf8872ae88d3d77f0f853
sejalkashyap97 <sejalkashyap97@gmail.com>
2026-01-10 16:46:57 +0530

    problem 5 added

---
+++ pblm5.py
@@ -0,0 +1,12 @@
+# Write a program to check whether a number given as user input is a power of 32 or
+# not. Accordingly, print POWER OF 32 or NOT POWER OF 32
+# def is_power_of_32(n):
+#     if n<=0:
+#         return False
+#     while n%32==0:
+#         n=n//32
+#     if n==1:
+#         return True
+#     else:
+#         return False
+print(31%32)
